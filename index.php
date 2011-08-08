fd7c23709f3a4dd0b6223ea679a7d1be9d65f877
Martin Giger <martin.a.giger@sunrise.ch>
2011-08-08 23:04:04 +0200

    * Overworked video loader * Fixed pagination (not final)

--- index.php
+++ index.php
@@ -17,8 +17,65 @@ $objResult = mysql_fetch_object($query);
 $G_ANALYTICS = $objResult->value;
 $query = mysql_query("SELECT value FROM settings WHERE name='lang'");
 $objResult = mysql_fetch_object($query);
-$PGLANG = $objResult->value;$cnt = 0;$count = 0;if($_GET['cat']) {	$q=1;	$quat=$_GET['cat'];}if($_GET['page']) {	$page=$_GET['page'];}else {	$page=1;}$categories[0]=placeholder;$query = mysql_query("SELECT name, url, caption, thumbnail, hello, category FROM content WHERE id='1'");$objResult = mysql_fetch_object($query);if($q==0||$objResult->category==$quat) {	if($objResult->caption!=NULL) {		$objcap=" - ".$objResult->caption;	}	else {		$objcap="";	}	if(($cnt<$PGITMS&&$page*$PGITMS>=$count&&$count>($page-1)*$PGITMS)||$PGITMS==0) {		$inshtml="<li><a href='".$objResult->url."' title='".$objResult->name.$objcap."'><span class='title'>".$objResult->name."</span><img src='".$objResult->thumbnail."' alt='".$objResult->name."'></a></li>";		$cnt++;	}	else {		$inshtml="";	}	$c=2;	$count= 1;}$categories[1]=$objResult->category;$d=2;$query = mysql_query("SELECT name, url, caption, thumbnail, hello, category FROM content WHERE id='$c'");$objResult = mysql_fetch_object($query);do {	if($objResult!=NULL&&($q==0||$objResult->category==$quat)) {		if($objResult->caption!=NULL) {			$objcap=" - ".$objResult->caption;		}		else {			$objcap="";		}		if(($cnt<$PGITMS&&$page*$PGITMS>=$count&&$count>($page-1)*$PGITMS)||$PGITMS==0) {			$inshtml=$inshtml."<li><a href='".$objResult->url."' title='".$objResult->name.$objcap."'><span class='title'>".$objResult->name."</span><img src='".$objResult->thumbnail."' alt='".$objResult->name."'></a></li>";			$cnt=$cnt+1;		}		$count=$count+1;	}	if(!(array_search($objResult->category,$categories))) {			$categories[$d]=$objResult->category;			$d=$d+1;	}	$c=$c+1;	$query = mysql_query("SELECT name, url, caption, thumbnail, hello, category FROM content WHERE id='$c'");	$objResult = mysql_fetch_object($query);} while($objResult->hello==1);$f=1;while($f<$d) {	if($quat==$categories[$f]) {		$class='actual';	}	else {		$class='';	}	$cat=$cat."<li><a href='?cat=".$categories[$f]."' class='".$class."' title='".$categories[$f]."'>".$categories[$f]."</a></li>";	$f=$f+1;}
-mysql_close($connect);?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd"><html><head><title><?php echo $PGNAME; ?> <?php if($_GET['cat']) { echo "- ".$_GET['cat']; } else {  echo "- ".$PGTEA; } ?></title>
+$PGLANG = $objResult->value;$cnt = 0; // items on this page
+$count = 1; // all items
+
+if($_GET['cat']) {
+	$q=1;
+	$quat=$_GET['cat'];
+}
+else {
+	$q=0;
+}
+
+if($_GET['page']) {
+	$page=$_GET['page'];
+}
+else {
+	$page=1;
+}
+
+$categories[0]="placeholder";
+
+$n = 1;
+$d = 1;
+$items;
+$query = mysql_query("SELECT name, url, caption, thumbnail, hello, category, date FROM content WHERE id='$n'");
+$objResult = mysql_fetch_object($query);
+do {
+	if($objResult!=NULL&&($q==0||$objResult->category==$quat)) {
+		if(($cnt<$PGITMS&&$page*$PGITMS>=$count&&$count>($page-1)*$PGITMS)||$PGITMS==0) {
+			if($objResult->caption!=NULL) {
+				$objcap=" - ".$objResult->caption;
+			}
+			else {
+				$objcap="";
+			}
+			$items[$cnt]["url"]=$objResult->url;
+			$items[$cnt]["name"]=$objResult->name;
+			$items[$cnt]["caption"]=$objResult->name.$objcap;
+			$items[$cnt]["thumbnail"]=$objResult->thumbnail;
+			$items[$cnt]["category"]=$objResult->category;
+			$items[$cnt]["date"]=$objResult->date;
+			$cnt=$cnt+1;
+		}
+		$count=$count+1;
+	}
+	if(!(array_search($objResult->category,$categories))||$d==1) {
+		$categories[$d]=$objResult->category;
+		$d=$d+1;
+	}
+	$n=$n+1;
+	$query = mysql_query("SELECT name, url, caption, thumbnail, hello, category, date FROM content WHERE id='$n'");
+	$objResult = mysql_fetch_object($query);
+}while($objResult->hello==1);
+mysql_close($connect);
+
+$inshtml="<li><a href='".$items[0]["url"]."' title='".$items[0]["caption"]."'><span class='title'>".$items[0]["name"]."</span><img src='".$items[0]["thumbnail"]."' alt='".$items[0]["name"]."'></a></li>";
+for($m=1;$m<$cnt;$m=$m+1) {
+	$inshtml=$inshtml."<li><a href='".$items[$m]["url"]."' title='".$items[$m]["caption"]."'><span class='title'>".$items[$m]["name"]."</span><img src='".$items[$m]["thumbnail"]."' alt='".$items[$m]["name"]."'></a></li>";
+}
+for($f=1;$f<$d;$f=$f+1) {	if($quat==$categories[$f]) {		$class='actual';	}	else {		$class='';	}	$cat=$cat."<li><a href='?cat=".$categories[$f]."' class='".$class."' title='".$categories[$f]."'>".$categories[$f]."</a></li>";}?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd"><html><head><title><?php echo $PGNAME; ?> <?php if($_GET['cat']) { echo "- ".$_GET['cat']; } else {  echo "- ".$PGTEA; } ?></title>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
 <meta name="description" content="<?php echo $PGDSC; ?>" >
 <meta name="keywords" content="<?php echo $PGTGS; ?>">
@@ -74,4 +131,4 @@ if(!($_GET['cat'])) { echo "<script type='text/javascript'>
 </script>";
 }
 ?></head><body>
-<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="login"></form><a id="loginlink" href="admin/login.php">Login</a> | <a href="impressum.php">Impressum</a> | <a href="<?php echo $PGURL; ?>/admin/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><p id="footer"><?php if($count>=$PGITMS&&$PGITMS!=0) {	$pgs=$count/$PGITMS;	if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2'>2</a> <a href='?page=".($pgs)."'>Letzte &gt;</a> | ";	}	else if($page<$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=2'>2</a> <a href='?page=".($page+1)."'>".($page+1)."</a> <a href='?page=".($pgs)."'>Letzte &gt;</a> | ";	}	else if($page=$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b> | ";	}}?></p></body></html>
+<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="login"></form><a id="loginlink" href="admin/login.php">Login</a> | <a href="impressum.php">Impressum</a> | <a href="<?php echo $PGURL; ?>/admin/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><p id="footer"><?php if($count>=$PGITMS&&$PGITMS!=0) {	$pgs=$count/$PGITMS;	if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2'>2</a> <a href='?page=".($pgs)."'>Letzte &gt;</a>";	}	else if($page<$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=2'>2</a> <a href='?page=".($page+1)."'>".($page+1)."</a> <a href='?page=".($pgs)."'>Letzte &gt;</a>";	}	else if($page=$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b>";	}}?></p></body></html>
