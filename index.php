ca2987db7ef7c051ba68ed1d57207abe4b0130d1
Martin Giger <martin.a.giger@sunrise.ch>
2011-09-11 15:40:49 +0200

    * Fixed pagination issues * Fixed category issues

--- index.php
+++ index.php
@@ -7,10 +7,14 @@ $inshtml="";
 $c=1;
 $d=1;
 $q=0;
-$categories[0]="placeholder";$cnt = 0; // items on this page
+$categories[0]="placeholder";$urlcatsuffix="";
+$cnt = 0; // items on this page
+
+
 
 if($_GET['cat']) {
 	$quat=$_GET['cat'];
+	$urlcatsuffix="&cat=".$quat;
 }
 
 if($_GET['page']) {
@@ -23,10 +27,11 @@ else {
 $inshtml="";
 
 for($inde=0;$inde<$items_length;$inde=$inde+1) {
-	if((!$quat||$items[$inde]["category"]==$quat)&&(($cnt<$PGITMS&&$page*$PGITMS>=$inde&&$inde>=($page-1)*$PGITMS)||$PGITMS==0)) {
+	if(($items[$inde]["category"]==$quat&&$cnt<$PGITMS)||(!$quat&&(($cnt<$PGITMS&&$page*$PGITMS>=$inde&&$inde>=($page-1)*$PGITMS)||$PGITMS==0))) {
 		$inshtml=$inshtml."<li><a href='".$items[$inde]["url"]."' title='".$items[$inde]["caption"]."'><span class='title'>".$items[$inde]["name"]."</span><img src='".$items[$inde]["thumbnail"]."' alt='".$items[$inde]["name"]."'></a></li>";
 		$cnt=$cnt+1;
 	}
+	
 	if(!(array_search($items[$inde]["category"],$categories))||$d==1) {
 		$categories[$d]=$items[$inde]["category"];
 		$d=$d+1;
@@ -91,5 +96,5 @@ if(!($_GET['cat'])) { echo "<script type='text/javascript'>
 </script>";
 }
 ?></head><body>
-<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="Log in"></form><a id="loginlink" href="admin/login.php">Log in</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><div id="bottom"><div id="pagination"><?php if($items_length>=$PGITMS&&$PGITMS!=0) {	$pgs=$items_length/$PGITMS;	if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2'>2</a> <a href='?page=".($pgs)."'>Letzte &gt;</a>";	}	else if($page<$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=2'>2</a> <a href='?page=".($page+1)."'>".($page+1)."</a> <a href='?page=".($pgs)."'>Letzte &gt;</a>";	}	else if($page=$pgs) {		echo "<a href='?page=1'>&lt; Erste</a> <a href='?page=".($page-1)."'>".($page-1)."</a> <b>".$page."</b>";	}}?></div>
+<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="Log in"></form><a id="loginlink" href="admin/login.php">Log in</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><div id="bottom"><div id="pagination"><?php if(!$quat&&$items_length>=$PGITMS&&$PGITMS!=0) {	$pgs=$items_length/$PGITMS;	if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2".$urlcatsuffix."'>2</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page<$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=".($page+1)."'>".($page+1)."</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page==$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b>";	}}?></div>
 <div id="footer"><a href="impressum.php">Impressum</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div></div></body></html>
