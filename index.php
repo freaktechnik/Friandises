6c6441fb270b4e500d1e3146243f9df220d2aac2
Martin Giger <martin.a.giger@sunrise.ch>
2011-10-07 20:11:35 +0200

    * Pagination works now for categorys

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $d=1;
 $q=0;
 $categories[0]="placeholder";$urlcatsuffix="";
 $cnt = 0; // items on this page
-
+$quatcount=0; // items in this category (usually 0)
 
 
 if($_GET['cat']) {
@@ -26,14 +26,27 @@ else {
 
 
 for($inde=0;$inde<$items_length;$inde=$inde+1) {
-	if(($items[$inde]["category"]==$quat&&$cnt<$PGITMS)||(!$quat&&(($cnt<$PGITMS&&$page*$PGITMS>=$inde&&$inde>=($page-1)*$PGITMS)||$PGITMS==0))) {
+	if(!$quat&&(($cnt<$PGITMS&&$page*$PGITMS>=$inde&&$inde>=($page-1)*$PGITMS)||$PGITMS==0)) {
 		$inshtml=$inshtml."<li><a href='/video.php?id=".$inde."' title='".$items[$inde]["caption"]."'><span class='title'>".$items[$inde]["name"]."</span><img src='".$items[$inde]["thumbnail"]."' alt='".$items[$inde]["name"]."'></a></li>";
 		$cnt=$cnt+1;
+	}
+	else if($items[$inde]["category"]==$quat) {
+		$quata[$quatcount]=$inde;
+		$quatcount=$quatcount+1;
 	}
 	
 	if(!(array_search($items[$inde]["category"],$categories))||$d==1) {
 		$categories[$d]=$items[$inde]["category"];
 		$d=$d+1;
+	}
+}
+
+if($quatcount>0) {
+	for($i=0;$i<$quatcount;$i=$i+1) {
+		if(($cnt<$PGITMS&&$page*$PGITMS>=$i&&$i>=($page-1)*$PGITMS)||$PGITMS==0) {
+			$inshtml=$inshtml."<li><a href='/video.php?id=".$i."' title='".$items[$quata[$i]]["caption"]."'><span class='title'>".$items[$quata[$i]]["name"]."</span><img src='".$items[$quata[$i]]["thumbnail"]."' alt='".$items[$quata[$i]]["name"]."'></a></li>";
+			$cnt=$cnt+1;
+		}
 	}
 }
 for($f=1;$f<$d;$f=$f+1) {	if($quat==$categories[$f]) {		$class='actual';	}	else {		$class='';	}	$cat=$cat."<li><a href='?cat=".$categories[$f]."' class='".$class."' title='".$categories[$f]."'>".$categories[$f]."</a></li>";}?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd"><html><head>
@@ -70,5 +83,11 @@ if(!($_GET['cat'])) { echo "<script type='text/javascript'>
 </script>";
 }
 ?></head><body>
-<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="Log in"></form><a id="loginlink" href="admin/login.php">Log in</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><div id="bottom"><div id="pagination"><?php if(!$quat&&$items_length>=$PGITMS&&$PGITMS!=0) {	$pgs=$items_length/$PGITMS;	if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2".$urlcatsuffix."'>2</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page<$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=".($page+1)."'>".($page+1)."</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page==$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b>";	}}?></div>
+<div id="topnav"><form method="POST" id="loginform" style="display:none;" action="admin/check.php">Username:<input type="text" name="name"/> | Password:<input type="password" name="passwort"/> <input type="submit" value="Log in"></form><a id="loginlink" href="admin/login.php">Log in</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div><div id="head"><a href="<?php echo $PGURL; ?>" style="text-decoration:none;"><?php if($PGTITLE==1) {	echo "<h1>".$PGNAME."</h1>";}else if($PGTITLE==2) {	echo "<img src='".$PGIMG."' alt='".$PGNAME."'>";}?></a><p><?php echo $PGTEA; ?></p></div><div id="navigation"><ul><li><a href="<?php echo $PGURL; ?>" class="<?php if(!($_GET['cat'])) { echo 'actual'; } ?>">Home</a></li><?php echo $cat; ?></ul></div><br><div id="videos"><ul><?php echo $inshtml; ?></ul></div><div id="bottom"><div id="pagination"><?php if((($items_length>=$PGITMS&&!$quat)||$quatcount>$PGITMS)&&$PGITMS!=0) {	if(!$quat) {
+		$pgs=$items_length/$PGITMS;
+	}
+	else {
+		$pgs=$quatcount/$PGITMS;
+	}
+		if($pgs-(int)$pgs!=0) {		$diff=$pgs-(int)$pgs;		$pgs=$pgs-$diff+1;	}	if($page==1) {		echo "<b>1</b> <a href='?page=2".$urlcatsuffix."'>2</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page<$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b> <a href='?page=".($page+1).$urlcatsuffix."'>".($page+1)."</a> <a href='?page=".($pgs).$urlcatsuffix."'>Letzte &gt;</a>";	}	else if($page==$pgs) {		echo "<a href='?page=1".$urlcatsuffix."'>&lt; Erste</a> <a href='?page=".($page-1).$urlcatsuffix."'>".($page-1)."</a> <b>".$page."</b>";	}}?></div>
 <div id="footer"><a href="impressum.php">Impressum</a> | <a href="<?php echo $PGURL; ?>/feeds/feed.rss" title="RSS Feed"><img src="images/rss.png" alt="RSS Feed" /></a></div></div></body></html>
